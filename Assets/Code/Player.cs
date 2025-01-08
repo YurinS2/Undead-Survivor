24fbed038ddcd9d98882926211a5b0e13e4bfe95
YurinS2 <nekaton15@gmail.com>
2025-01-09 03:54:17 +0900

    20250109 AM 3:54

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -8,9 +8,13 @@ public class Player : MonoBehaviour
     public Vector2 inputVec;
     public float speed;
     Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     void FixedUpdate()
@@ -22,5 +26,15 @@ public class Player : MonoBehaviour
     void OnMove(InputValue value)
     {
         inputVec = value.Get<Vector2>();
+    }
+
+    void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        if (inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
     }
 }
